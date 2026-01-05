3c920d4a21782ecde56127903427cfb31ef4c129
hellohirusha <randombage.hirusha@gmail.com>
2026-01-05 08:55:51 +0530

    chore: wrap App.tsx with OrdersProvider for global order state

--- App.tsx
+++ App.tsx
@@ -3,17 +3,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { CartProvider } from './src/context/CartContext';
 import BuyerStack from './src/navigation/BuyerStack';
+import { OrdersProvider } from './src/context/OrdersContext';
 
 const RootStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <CartProvider>
+      <OrdersProvider>
       <NavigationContainer>
         <RootStack.Navigator screenOptions={{ headerShown: false }}>
           <RootStack.Screen name="BuyerRoot" component={BuyerStack} />
         </RootStack.Navigator>
       </NavigationContainer>
+      </OrdersProvider>
     </CartProvider>
   );
 }
